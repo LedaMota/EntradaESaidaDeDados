cf1d055e855c156d99a4228b019399dc41b8ca77
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-08-24 18:14:57 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrada_Saida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Entrada e saida
+            //    //Aqui o console.WriteLine esta quebrando uma linha e exibindo uma informaçao que é uma saida de dados.
+            //     Console.WriteLine("Leidiane");  ///SAida
+
+            //    //Aqui o console nao quebra a linha esta exibindo a informaçao com auma saida de dados.
+            //     Console.Write("Mota"); ///SAida
+
+            //    //Aqui eu declarei uma variavel com o nome da variavel condigo com o tipo de inteiro e estou atribuindo essa variavel uma entrada de dado "Console.Read();"
+            //    int codigo = Console.Read();//Entrada
+
+            //    //Aqui o "Console.WriteLine" está exibindo uma saida de dados da variavel na tela para o usuario.
+            //    Console.WriteLine(codigo); ///SAida
+
+            //    string texto = Console.ReadLine();
+            //    Console.WriteLine(texto); ///SAida
+
+            //    Console.ReadKey();
+
+            //string nome;
+            //int idade;
+            //decimal altura;
+
+            //Console.WriteLine("====== Escreva o nome =======");
+            //nome = Console.ReadLine();
+
+            //Console.WriteLine("Digite a sua idade");
+            //idade = Convert.ToInt32 (Console.ReadLine());
+
+            //Console.WriteLine("Digite a sua Altura");
+            //altura = Convert.ToDecimal(Console.ReadLine());
+
+            //Console.WriteLine("====== Dados =======");
+
+            //Console.WriteLine("O meu nome é:"+nome+ " e a minha idade é:" +idade+ "com altura:" +altura);
+
+            //Console.ReadKey();
+            #endregion
+            
+        }
+
+    }
+}
